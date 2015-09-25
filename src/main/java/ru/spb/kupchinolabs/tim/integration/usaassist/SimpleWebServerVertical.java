f5a0d381ab03486033c88df7ef338df70f7263e3
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2015-09-25 20:00:01 +0300

    adding form, webserver

---
+++ src/main/java/ru/spb/kupchinolabs/tim/integration/usaassist/SimpleWebServerVertical.java
@@ -0,0 +1,71 @@
+package ru.spb.kupchinolabs.tim.integration.usaassist;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+
+import java.util.Set;
+
+public class SimpleWebServerVertical extends AbstractVerticle {
+
+    // Convenience method so you can run it in your IDE
+    public static void main(String[] args) {
+        System.setProperty("vertx.cwd", "target/classes");
+        System.setProperty("vertx.disableFileCaching", "true");
+        Vertx.vertx().deployVerticle(new SimpleWebServerVertical());
+    }
+
+    @Override
+    public void start() throws Exception {
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+        SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+        BridgeOptions bridgeOptions = new BridgeOptions()
+//                .addOutboundPermitted(new PermittedOptions().setAddress("response"))
+                .addInboundPermitted(new PermittedOptions().setAddress("request"));
+        sockJSHandler.bridge(bridgeOptions);
+        router.route("/eventbus/*").handler(sockJSHandler);
+        router.route().handler(StaticHandler.create());
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+        vertx.eventBus().consumer("request", this::handleRequest);
+    }
+
+    private void handleRequest(Message<JsonObject> message) {
+        final JsonObject jsonObject = message.body();
+        //TODO add params from request
+        HttpClientOptions options = new HttpClientOptions().setDefaultHost("www.usa-assist.com");
+        HttpClient client = vertx.createHttpClient(options);
+        Buffer buffer = Buffer.buffer()
+                .appendString("affiliate=669").appendString("&")
+                .appendString("password=travel123").appendString("&")
+                .appendString("test=true").appendString("&");
+        final Set<String> fieldNames = jsonObject.fieldNames();
+        for (String key: fieldNames){
+            final String value = (String) jsonObject.getValue(key);
+            buffer.appendString(key).appendString("=").appendString(value).appendString("&");
+        }
+        String body = buffer.toString();
+        client.request(HttpMethod.POST, "/modules/transact", response -> {
+            System.out.println(response.statusMessage());
+            System.out.println(response.statusCode());
+            response.handler(bfr -> {
+                final JsonObject json = new JsonObject(bfr.toString());
+                System.out.println(json.encodePrettily());
+                message.reply(json);
+            });
+        }).putHeader("Content-Type", "application/x-www-form-urlencoded")
+                .putHeader("Content-Language", "en-US")
+                .end(body);
+    }
+}
